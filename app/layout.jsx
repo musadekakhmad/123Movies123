95a196d8f3e5ea0853788125049b8c7f2b3b1e27
musadek akhmad <musadekakhmad@gmail.com>
2025-09-15 05:54:16 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -39,6 +39,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Tag verifikasi Google Search Console */}
+        <meta name="google-site-verification" content="43U0FE_9YdrByOFa7zNlMk0hxaMxt1P1zitJs_LBJFU" />
+      </head>
       <body>
         <AdsterraLayoutWrapper>
           <div className="flex flex-col min-h-screen bg-slate-900">
@@ -50,7 +54,7 @@ export default function RootLayout({ children }) {
             </main>
             <footer className="w-full max-w-7xl mx-auto px-4 py-8">
               {/* Tempatkan div Native Banner di sini, sebelum Footer */}
-              <div id="container-71b765f9f86e0b42fb5040978bef3bc2"></div>
+              <div id="container-ce4c42ba51eddb0024dfa25613d99fda"></div>
               <Footer />
             </footer>
           </div>
